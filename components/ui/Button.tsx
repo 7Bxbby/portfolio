120b322874e32d6f0f01a8cdc6fa28fcaf987be2
bxbby <bxbbyafterhours@gmail.com>
2025-03-17 17:10:55 +0100

    naprawione bledy z typami

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -22,12 +22,11 @@ export function Button({
                        }: {
     borderRadius?: string;
     children: React.ReactNode;
-    as?: any;
+    as?: React.ElementType;
     containerClassName?: string;
     borderClassName?: string;
     duration?: number;
     className?: string;
-    [key: string]: any;
 }) {
     return (
         <Component
@@ -80,10 +79,8 @@ export const MovingBorder = ({
     duration?: number;
     rx?: string;
     ry?: string;
-    [key: string]: any;
 }) => {
-    // @ts-expect-error
-    const pathRef = useRef<any>();
+    const pathRef = useRef<SVGRectElement>(null);
     const progress = useMotionValue<number>(0);
 
     useAnimationFrame((time) => {
